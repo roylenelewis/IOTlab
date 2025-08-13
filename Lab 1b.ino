13736ac6b6426a674329a9e1a9d23c8f37fe9271
roylenelewis <162871835+roylenelewis@users.noreply.github.com>
2025-08-13 15:52:26 +0530

    Create Lab 1b.ino

---
+++ Lab 1b.ino
@@ -0,0 +1,30 @@
+// C++ code
+//
+
+int leds[]={2,3,4,5,6};
+void setup()
+{
+  for(int i=0;i<5;i++){
+    pinMode(i, OUTPUT);
+   }
+}
+
+void loop()
+{
+  for(int i=0;i<5;i++){
+  digitalWrite(leds[i], HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(leds[i], LOW);
+  delay(200); // Wait for 1000 millisecond(s)
+  
+  }
+  
+  for(int i=5;i>0;i--){
+  digitalWrite(leds[i], HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(leds[i], LOW);
+  delay(200); // Wait for 1000 millisecond(s)
+  
+  }
+  
+}
